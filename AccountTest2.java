f7e52ed3419c4e90db0f04e3d2b4a54af7a9ebc1
Sepvaldi <sepvalditunisia@gmail.com>
2025-08-21 07:10:33 +0700

    Add files via upload

---
+++ AccountTest2.java
@@ -0,0 +1,26 @@
+package AccountTest;
+
+public class AccountTest2 {
+    public static void main(String[] args) {
+        SavingsAccount2 sa0001 = new SavingsAccount2();
+        sa0001.balance = 1000;
+        sa0001.name = "Damien";
+        sa0001.interestRate = 0.02;
+        sa0001.displayCustomer();
+        sa0001.deposit(500);
+        System.out.println("--------------------------");
+
+        SavingsAccount2 sa0002 = new SavingsAccount2();
+        sa0002.balance = 2000;
+        sa0002.name = "Bill";
+        sa0002.interestRate = 0.07;
+        sa0002.displayCustomer();
+        sa0002.deposit(1000);
+        System.out.println("----------------------------");
+
+    }
+        
+}
+
+
+
---
+++ SavingsAccount2.java
@@ -0,0 +1,19 @@
+package AccountTest;
+
+public class SavingsAccount2 {
+    public double interestRate;
+    public double balance;
+    public String name;
+
+    public void displayCustomer(){
+        System.out.println("Customer: " + name);
+        System.out.println("Balance: " + balance);
+        System.out.println("Rate: " + interestRate);
+    }
+        public void deposit(int x){
+        balance += x;
+    }
+        public double calcInterest(){
+        return balance * interestRate;
+    }
+}
